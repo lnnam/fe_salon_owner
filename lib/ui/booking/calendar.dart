4f8fd759ee1d152c91fdbebcf10f20d6f621bcc1
lnnam <lnnam.itwork@gmail.com>
2024-05-16 13:40:58 +0100

    ok

--- lib/ui/booking/calendar.dart
+++ lib/ui/booking/calendar.dart
@@ -1,53 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:booking_calendar/booking_calendar.dart';
+import 'package:salonapp/constants.dart';
 
-class BookingCalendarPage extends StatelessWidget {
-  final List<Booking> bookings = [
-    Booking(DateTime.now(), DateTime.now().add(Duration(days: 2))),
-    Booking(DateTime.now().add(Duration(days: 5)), DateTime.now().add(Duration(days: 7))),
-  ];
+class BookingCalendarPage extends StatefulWidget {
+  @override
+  _BookingCalendarPageState createState() => _BookingCalendarPageState();
+}
+
+class _BookingCalendarPageState extends State<BookingCalendarPage> {
+  late BookingService mockBookingService;
+  final now = DateTime.now();
+  List<DateTimeRange> converted = [];
+
+  @override
+  void initState() {
+    super.initState();
+    mockBookingService = BookingService(
+        serviceName: 'Mock Service',
+        serviceDuration: 15,
+        bookingEnd: DateTime(now.year, now.month, now.day, 18, 0),
+        bookingStart: DateTime(now.year, now.month, now.day, 9, 0));
+  }
+
+  Stream<dynamic>? getBookingStreamMock(
+      {required DateTime end, required DateTime start}) {
+    return Stream.value([]);
+  }
+
+  Future<dynamic> uploadBookingMock(
+      {required BookingService newBooking}) async {
+    await Future.delayed(const Duration(seconds: 1));
+    converted.add(DateTimeRange(
+        start: newBooking.bookingStart, end: newBooking.bookingEnd));
+    print('${newBooking.toJson()} has been uploaded');
+  }
+
+  List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
+    DateTime first = now;
+    DateTime tomorrow = now.add(const Duration(days: 1));
+    DateTime second = now.add(const Duration(minutes: 55));
+    DateTime third = now.subtract(const Duration(minutes: 240));
+    DateTime fourth = now.subtract(const Duration(minutes: 500));
+    converted.add(
+        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
+    converted.add(DateTimeRange(
+        start: second, end: second.add(const Duration(minutes: 23))));
+    converted.add(DateTimeRange(
+        start: third, end: third.add(const Duration(minutes: 15))));
+    converted.add(DateTimeRange(
+        start: fourth, end: fourth.add(const Duration(minutes: 50))));
+
+    //book whole day example
+    converted.add(DateTimeRange(
+        start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
+        end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
+    return converted;
+  }
+
+  List<DateTimeRange> generatePauseSlots() {
+    return [
+      DateTimeRange(
+          start: DateTime(now.year, now.month, now.day, 12, 0),
+          end: DateTime(now.year, now.month, now.day, 13, 0))
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
+    final color = Color(COLOR_PRIMARY);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Booking Calendar Page'),
-      ),
-      body: Center(
-        child: BookingCalendar(
-          startDate: DateTime.now(),
-          endDate: DateTime.now().add(Duration(days: 365)),
-          bookingService: _bookingService,
-          onDateChange: (date) {
-            print('Selected date: $date');
-          },
-          onEventTap: (event) {
-            print('Tapped event: $event');
-          },
-          onMonthChange: (month) {
-            print('Month changed: $month');
-          },
-          onRangeSelected: (range) {
-            print('Selected range: $range');
-          },
-          dayOfWeekStyle: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+           title: const Text('Booking Calendar',  style: TextStyle(color: Colors.white)),
+           backgroundColor: color, // Set app bar color
+        ),
+        body: Center(
+          child: BookingCalendar(
+            bookingService: mockBookingService,
+            convertStreamResultToDateTimeRanges: convertStreamResultMock,
+            getBookingStream: getBookingStreamMock,
+            uploadBooking: uploadBookingMock,
+            pauseSlots: generatePauseSlots(),
+            pauseSlotText: 'LUNCH',
+            hideBreakTime: false,
+            loadingWidget: const Text('Fetching data...'),
+            uploadingWidget: const CircularProgressIndicator(),
+            locale: 'en_GB',
+            startingDayOfWeek: StartingDayOfWeek.tuesday,
+            wholeDayIsBookedWidget:
+                const Text('Sorry, for this day everything is booked'),
+            //disabledDates: [DateTime(2023, 1, 20)],
+            //disabledDays: [6, 7],
           ),
         ),
-      ),
-    );
+      );
   }
-
-  List<Booking> _bookingService(DateTime start, DateTime end) {
-    // Replace this with your own logic to fetch bookings within the specified range
-    return bookings.where((booking) => booking.start.isAfter(start) && booking.end.isBefore(end)).toList();
-  }
-}
-
-class Booking {
-  final DateTime start;
-  final DateTime end;
-
-  Booking(this.start, this.end);
 }
